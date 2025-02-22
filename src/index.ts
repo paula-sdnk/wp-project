b7517185be3970a7112b9947a73b5bfabf5463d1
Paula Sadenok <paula.saden@gmail.com>
2025-02-22 22:20:22 +0200

    Updated index.ts

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,9 @@ const server = Bun.serve({
   port: 3000,
   hostname: "0.0.0.0",
   fetch(req) {
-    return new Response("Hello from Bun server in Docker!");
+    return new Response("Hello from Bun server in Docker!", {
+      headers: { "Content-Type": "text/plain" },
+    });
   },
 });
 
