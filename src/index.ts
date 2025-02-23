1367075c50cdcaaa767f7ca3a8679a6eea889024
Paula Sadenok <paula.saden@gmail.com>
2025-02-23 16:03:05 +0200

    Added Dockerfile

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,7 @@ const server = Bun.serve({
   port: 3000,
   hostname: "0.0.0.0",
   fetch(req) {
-    return new Response("Hello from Bun server in Docker!", {
-      headers: { "Content-Type": "text/plain" },
-    });
+    return new Response("Hello from Bun server in Docker!");
   },
 });
 
